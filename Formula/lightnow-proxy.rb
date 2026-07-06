9c9e2bbd1660b78ed3832d31dfc021d85e08f126
Nils Friedrichs <nils@friedrichs-it.de>
2026-07-06 18:24:28 +0200

    Update lightnow-proxy test to 1.1.0

--- Formula/lightnow-proxy.rb
+++ Formula/lightnow-proxy.rb
@@ -227,6 +227,6 @@ class LightnowProxy < Formula
   end
 
   test do
-    assert_match "lightnow-proxy 1.0.1", shell_output("#{bin}/lightnow-proxy --version")
+    assert_match "lightnow-proxy 1.1.0", shell_output("#{bin}/lightnow-proxy --version")
   end
 end
